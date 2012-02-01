438c911ef036ea8e1c5b302be790ba08e24c5ca0
GenaSG <henadiy.shvetsov@gmail.com>
2012-02-01 14:48:22 +0200

    fix for Unranked/makeMod.sh

--- Unranked/makeMod.sh
+++ Unranked/makeMod.sh
@@ -2,11 +2,11 @@
 rm -f z_ProjectileWeapons.iwd
 rm -f mod.ff
 
-cp -rf ./ui_mp ../../raw/ui_mp
-cp -f mod.csv ../../zone_source
+cp -rf ./ui_mp ../../raw/
+cp -f mod.csv ../../zone_source/
 cd ../../bin/
 wine linker_pc.exe -language english -compress -cleanup mod
 cd ../Mods/Unranked
-cp ../../zone/english/mod.ff ./
+cp -f ../../zone/english/mod.ff ./
 
 
